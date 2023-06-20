59cb05d3e1eee10cc860456d290d790f96480afc
Jean-Lucas-B <jeanlucasb8@gmail.com>
2023-06-20 05:35:13 -0300

    Sorteador-Dev

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function gerarCodigos(){
-    
-    const min = Math.ceil(document.querySelector(".luck1").value)
-    const max =  Math.floor(document.querySelector(".luck2").value)
-    
-    const result =  Math.floor(Math.random() * (max - min + 1)) + min;
+function gerarCodigos() {
 
-    alert(result)
+    const min = Math.ceil(document.querySelector(".luck1").value)
+    const max = Math.floor(document.querySelector(".luck2").value)
+
+    if (min >= max) {
+        alert("O valor minimo tem que ser MENOR que o valor máximo")
+
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
+
 }
